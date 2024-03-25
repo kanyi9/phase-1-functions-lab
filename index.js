be3d1db9b472190c05c3b3da434e071db6644795
Peter <peterkanyi19193@gmail.com>
2024-03-25 10:40:50 +0300

    add functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNo) {
+   const distance = Math.abs(streetNo - 42) //returns the number of blocks given a value
+    return distance;
+}
+
+function distanceFromHqInFeet(streetNo) {
+    const blocks = distanceFromHqInBlocks(streetNo);
+    return blocks*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled= Math.abs(destination - start);
+    return blocksTravelled* 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet= distanceTravelledInFeet(start, destination)
+    if (distanceInFeet<400){
+        return 0;
+    }else if (distanceInFeet<2000){
+        return (0.02 *(distanceInFeet-400));
+
+    }else if(distanceInFeet>2000 && distanceInFeet<2500){
+        return 25
+    }else{
+        return "cannot travel that far"}
+        
+    }
+    
+  
+
+     
+    
